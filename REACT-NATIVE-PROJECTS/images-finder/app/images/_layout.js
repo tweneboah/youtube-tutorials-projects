f0d30bbf5d6ae1133b8bf08dd91024a2642865e1
Masynctech <twentekghana@gmail.com>
2024-07-12 08:03:19 +0000

    React native photo app

---
+++ REACT-NATIVE-PROJECTS/images-finder/app/images/_layout.js
@@ -0,0 +1,10 @@
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { Stack } from "expo-router";
+
+export default function RootLayout() {
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Stack />
+    </GestureHandlerRootView>
+  );
+}
